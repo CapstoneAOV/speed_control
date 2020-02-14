d2c9edc8f0aecabb37f840a403db47a99acd841e
harjeevanmaan <harjeevanm@gmail.com>
2020-02-14 10:53:12 -0500

    setting speed to 0 now enables braking, pos/neg values now switch directions

--- scripts/speed_control.py
+++ scripts/speed_control.py
@@ -28,23 +28,23 @@ def writeout(cmd):
     return response
     
 
-def callback_direction(data):
-    print("callback direction")
-    pub = rospy.Publisher("Arduino_response", std_msgs.msg.String, queue_size=10)
-    rospy.Rate(10)
+# def callback_direction(data):
+#     print("callback direction")
+#     pub = rospy.Publisher("Arduino_response", std_msgs.msg.String, queue_size=10)
+#     rospy.Rate(10)
 
-    cmd = data.data
-    if(cmd == "forward"):
-        writeout("0.0000")              #disconnect and wait, to prevent shorting of forward/reverse
-        time.sleep(0.1)
-        response = writeout("0.0010")
-    elif(cmd == "reverse"):
-        writeout("0.0000")              #consider adding state variable to monitor forward/reverse?
-        time.sleep(0.1)
-        response = writeout("0.0001")       #turn velocity to zero when changing direction?
-    else:
-        response = "Invalid Command"      #also need to publish to error topic, error code for direction switching
-    pub.publish(response)
+#     cmd = data.data
+#     if(cmd == "forward"):
+#         writeout("0.0000")              #disconnect and wait, to prevent shorting of forward/reverse
+#         time.sleep(0.1)
+#         response = writeout("0.0010")
+#     elif(cmd == "reverse"):
+#         writeout("0.0000")              #consider adding state variable to monitor forward/reverse?
+#         time.sleep(0.1)
+#         response = writeout("0.0001")       #turn velocity to zero when changing direction?
+#     else:
+#         response = "Invalid Command"      #also need to publish to error topic, error code for direction switching
+#     pub.publish(response)
 
 def callback_speed(data):
     print("Callback speed")
@@ -52,8 +52,16 @@ def callback_speed(data):
     rospy.Rate(10)
 
     speed = data.data
-    if(speed > 5 or speed < 0):      # conversion of speed to volts?
+    print("speed is {0}".format(speed))
+    if(speed > 5 or speed < -0.5):      # conversion of speed to volts?
         response = "FAIL: invalid parameters"
+
+    elif(speed >-0.2 and speed < 0.1):
+        print("turning on brakes")
+        response = writeout(str(0.0) + "xxx")
+        braking_pub = rospy.Publisher('Braking', std_msgs.msg.Bool, queue_size=10)
+        print(braking_pub)
+        braking_pub.publish(True)
     
     else:
         volts = round(speed,1)      
@@ -63,11 +71,12 @@ def callback_speed(data):
 
 def listener():
     print("entered listener")
-    rospy.Subscriber("Direction", std_msgs.msg.String, callback_direction)      #can change to int
+    # rospy.Subscriber("Direction", std_msgs.msg.String, callback_direction)      #can change to int
     rospy.Subscriber("Speed", std_msgs.msg.Float32, callback_speed)
     rospy.spin()
 
 def main():
+    print("main entered")
     rospy.init_node('Relay_Speed_Control', anonymous=True)
     port = rospy.get_param("~port")
     baudrate = int(rospy.get_param("~baudrate"))
